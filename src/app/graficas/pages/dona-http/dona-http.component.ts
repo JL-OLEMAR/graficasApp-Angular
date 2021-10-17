99a89ba35ca06729bc7ea362841447081e9ef1b4
JL-OLEMAR <joseluis19963@gmail.com>
2021-10-17 04:18:18 -0500

    Add grafica dona mediante peticion http

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,14 +1,40 @@
 import { Component, OnInit } from '@angular/core'
+import { ChartType } from 'chart.js'
+import { Color, Label, MultiDataSet } from 'ng2-charts'
+import { GraficasService } from '../../services/graficas.service'
 
 @Component({
   selector: 'app-dona-http',
   templateUrl: './dona-http.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class DonaHttpComponent implements OnInit {
-  constructor () { }
+  doughnutChartLabels: Label[] = []
+  doughnutChartData: MultiDataSet = []
+  doughnutChartType: ChartType = 'doughnut'
+  colors: Color[] = [
+    {
+      backgroundColor: [
+        '#0075ED',
+        '#00BAF7',
+        '#00E0DB',
+        '#00F7AD',
+        '#00ED63'
+      ]
+    }
+  ]
+
+  constructor (private readonly graficasService: GraficasService) {}
 
   ngOnInit (): void {
+    this.graficasService.getUsuariosForDonaCharts()
+      .subscribe(({ labels, values }) => {
+        this.doughnutChartLabels = labels
+        this.doughnutChartData.push(values)
+      })
+
+    // .subscribe((data) => {
+    //   console.log(data)
+    // })
   }
 }
--- src/app/graficas/services/graficas.service.ts
+++ src/app/graficas/services/graficas.service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { delay, map } from 'rxjs/operators'
+
+interface SocialMedia {
+  facebook: number
+  youtube: number
+  whatsapp: number
+  'facebook-messenger': number
+  instagram: number
+}
+
+interface DataResponse {
+  labels: string[]
+  values: number[]
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class GraficasService {
-  constructor () { }
+  constructor (private readonly http: HttpClient) { }
+
+  getUsuarios (): Observable<SocialMedia> {
+    return this.http.get<SocialMedia>('http://localhost:3000/grafica')
+  }
+
+  getUsuariosForDonaCharts (): Observable<DataResponse> {
+    return this.getUsuarios()
+      .pipe(
+        delay(1500),
+        map(data => {
+          const labels = Object.keys(data)
+          const values = Object.values(data)
+          return { labels, values }
+        })
+      )
+  }
 }
