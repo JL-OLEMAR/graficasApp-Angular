b769b6fba95b291c494f8684c0e287f16f68334e
JL-OLEMAR <joseluis19963@gmail.com>
2021-10-16 23:54:31 -0500

    Rutas y lazyload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: 'graficas',
+    loadChildren: async () => await import('./graficas/graficas.module').then(m => m.GraficasModule)
+  },
+  { path: '**', redirectTo: 'graficas' }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
