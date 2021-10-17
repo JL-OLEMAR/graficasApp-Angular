bcd2843b7242eb3ce620b63e5eb8d76206c1b5c0
JL-OLEMAR <joseluis19963@gmail.com>
2021-10-17 01:37:11 -0500

    Add grafica de barra

--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -1,14 +1,56 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js'
+import { Label } from 'ng2-charts'
 
 @Component({
   selector: 'app-barras',
   templateUrl: './barras.component.html',
-  styles: [
-  ]
+  styles: []
 })
-export class BarrasComponent implements OnInit {
-  constructor () { }
+export class BarrasComponent {
+  barChartOptions: ChartOptions = {
+    responsive: true
+  }
 
-  ngOnInit (): void {
+  barChartLabels: Label[] = ['2020', '2021', '2022', '2023', '2024', '2025', '2026']
+  barChartType: ChartType = 'bar'
+  barChartLegend = true
+
+  barChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#ED5F76', hoverBackgroundColor: 'red' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#F763C4', hoverBackgroundColor: 'red' },
+    { data: [8, 38, 70, 59, 66, 80, 100], label: 'Series C', backgroundColor: '#D665E0', hoverBackgroundColor: 'red' }
+  ]
+
+  randomize (): void {
+    this.barChartData[0].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100)
+    ]
+
+    this.barChartData[1].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100)
+    ]
+
+    this.barChartData[2].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100)
+    ]
   }
 }
