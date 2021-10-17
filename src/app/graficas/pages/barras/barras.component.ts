f701dc21576c6d304282f2f073234d61d6a8c192
JL-OLEMAR <joseluis19963@gmail.com>
2021-10-16 23:21:37 -0500

    Estructura del proyecto

---
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-barras',
+  templateUrl: './barras.component.html',
+  styles: [
+  ]
+})
+export class BarrasComponent implements OnInit {
+  constructor () { }
+
+  ngOnInit (): void {
+  }
+}
